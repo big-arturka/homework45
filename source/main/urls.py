7f3abf162d18a26740e02dba19ecfe674647db41
Arthur <arturkrmnlv10@gmail.com>
2020-08-05 19:18:26 +0600

    Представления переделаны на классовые представления, изменнены url-адреса с использованием классовых представлений.

--- source/main/urls.py
+++ source/main/urls.py
@@ -15,14 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from webapp.views import index_view, task_create_view, task_view, task_update_view, task_delete_view, bulk_delete_view
+from webapp.views import IndexView, TaskCreateView, TaskView, TaskUpdateView, TaskDeleteView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index_view, name='index'),
-    path('task/add/', task_create_view, name='task_create'),
-    path('task/<int:pk>/', task_view, name='task_view'),
-    path('task/<int:pk>/edit/', task_update_view, name='task_update'),
-    path('task/<int:pk>/delete/', task_delete_view, name='task_delete'),
-    path('task/bulk_delete/', bulk_delete_view, name='bulk_delete'),
+    path('', IndexView.as_view(), name='index'),
+    path('task/add/', TaskCreateView.as_view(), name='task_create'),
+    path('task/<int:pk>/', TaskView.as_view(), name='task_view'),
+    path('task/<int:pk>/edit/', TaskUpdateView.as_view(), name='task_update'),
+    path('task/<int:pk>/delete/', TaskDeleteView.as_view(), name='task_delete'),
 ]
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -2,71 +2,76 @@ from django.shortcuts import render, redirect, get_object_or_404
 from webapp.models import Task
 from django.http import HttpResponseNotAllowed
 from webapp.forms import TaskForm
+from django.views.generic import View, TemplateView
 
 
-def index_view(request):
-    data = Task.objects.all()
-    return render(request, 'index.html', context={
-        'tasks': data
-    })
+class IndexView(TemplateView):
+    template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        task = Task.objects.all()
+
+        context['tasks'] = task
+        return context
 
 
-def task_view(request, pk):
-    task = get_object_or_404(Task, pk=pk)
-    return render(request, 'task_view.html', context={
-        'task': task})
+class TaskView(TemplateView):
+    template_name = 'task_view.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        pk = self.kwargs.get('pk')
+        task = get_object_or_404(Task, pk=pk)
+        context['task'] = task
+        return context
 
 
-def task_create_view(request):
-    if request.method == 'GET':
+class TaskCreateView(View):
+    def get(self, request):
         return render(request, 'task_create.html',
                       context={'form': TaskForm()})
-    elif request.method == 'POST':
+
+    def post(self, request):
         form = TaskForm(data=request.POST)
         if form.is_valid():
             task = Task.objects.create(**form.cleaned_data)
             return redirect('task_view', pk=task.pk)
         else:
             return render(request, 'task_create.html', context={'form': form})
-    else:
-        return HttpResponseNotAllowed(permitted_methods=['GET', 'POST'])
 
 
-def task_update_view(request, pk):
-    task = get_object_or_404(Task, pk=pk)
-    if request.method == 'GET':
+class TaskUpdateView(View):
+    def get(self, request, pk):
+        task = get_object_or_404(Task, pk=pk)
         form = TaskForm(initial={'title': task.title,
                                  'description': task.description,
                                  'status': task.status,
-                                 'completion_date': task.completion_date})
+                                 'type': task.type})
         return render(request, 'task_update.html', context={'form': form, 'task': task})
-    elif request.method == 'POST':
+
+    def post(self, request, pk):
+        task = get_object_or_404(Task, pk=pk)
         form = TaskForm(data=request.POST)
         if form.is_valid():
             task.title = form.cleaned_data['title']
             task.status = form.cleaned_data['status']
             task.description = form.cleaned_data['description']
-            task.completion_date = form.cleaned_data['completion_date']
+            task.type = form.cleaned_data['type']
             task.save()
             return redirect('task_view', pk=task.pk)
         else:
             return render(request, 'task_update.html',
                           context={'form': form,
                                    'task': task})
-    else:
-        return HttpResponseNotAllowed(permitted_methods=['GET', 'POST'])
 
 
-def task_delete_view(request, pk):
-    task = get_object_or_404(Task, pk=pk)
-    if request.method == 'GET':
+class TaskDeleteView(View):
+    def get(self, request, pk):
+        task = get_object_or_404(Task, pk=pk)
         return render(request, 'task_delete.html', context={'task': task})
-    elif request.method == 'POST':
+
+    def post(self, request):
+        task = get_object_or_404(Task, pk=pk)
         task.delete()
         return redirect('index')
-
-
-def bulk_delete_view(request):
-    Task.objects.filter(id__in=request.POST.getlist('item')).delete()
-    return redirect('index')
-
