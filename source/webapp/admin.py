c8f5bafcd7e18c29d68907ea7cdff89426b1d465
Arthur <arturkrmnlv10@gmail.com>
2020-08-05 17:01:30 +0600

    Созданы модели и зарегистрированы в админке. Проведены миграции.

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
-from webapp.models import Task
+from webapp.models import Task, Status, Task_type
 
 admin.site.register(Task)
+admin.site.register(Task_type)
+admin.site.register(Status)
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,13 +1,41 @@
 from django.db import models
 
 STATUS_CHOICES = [('new', 'Новая'), ('in_progress', 'В процессе'),  ('done', 'Сделано')]
+TYPE_CHOICES = [('task', 'Задача'), ('bug', 'Ошибка'), ('enhancement', 'Улучшение')]
+
+
+class Status(models.Model):
+    status = models.CharField(max_length=15, choices=STATUS_CHOICES, default='new', verbose_name='Статус')
+
+    def __str__(self):
+        return "{}.".format(self.status)
+
+    class Meta:
+        verbose_name = 'Статус'
+        verbose_name_plural = 'Статусы'
+
+
+class Task_type(models.Model):
+    type = models.CharField(max_length=15, choices=TYPE_CHOICES, default='task', verbose_name='Тип')
+
+    def __str__(self):
+        return "{}.".format(self.type)
+
+    class Meta:
+        verbose_name = 'Тип'
+        verbose_name_plural = 'Типы'
 
 
 class Task(models.Model):
     title = models.CharField(max_length=200, verbose_name='Описание')
     description = models.TextField(max_length=200, null=True, blank=True, verbose_name='Подробное описание')
-    status = models.TextField(max_length=3000, verbose_name='Статус', default='new', choices=STATUS_CHOICES)
-    completion_date = models.DateField(auto_now_add=False, null=True, blank=True, verbose_name='Время завершения')
+    status = models.ForeignKey('webapp.Status', related_name='statuses', on_delete=models.PROTECT, verbose_name='Статус')
+    type = models.ForeignKey('webapp.Task_type', related_name='types', on_delete=models.PROTECT, verbose_name='Тип')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
 
     def __str__(self):
         return "{}. {}".format(self.pk, self.title)
+
+    class Meta:
+        verbose_name = 'Задача'
+        verbose_name_plural = 'Задачи'
