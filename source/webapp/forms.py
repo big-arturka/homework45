0fad09d0f66947e9120ea5d0d5f3d2ea7ee60e61
Arthur <arturkrmnlv10@gmail.com>
2020-08-11 20:45:13 +0600

    Форма модели задачи переделана на модельную форму.

--- source/webapp/forms.py
+++ source/webapp/forms.py
@@ -1,14 +1,22 @@
 from django import forms
+from webapp.models import Status, Task_type, Task
 
-from webapp.models import Status, Task_type
 
+class TaskForm(forms.ModelForm):
+    class Meta:
+        model = Task
+        fields = ['title', 'description', 'status', 'task_type']
+        widgets = {'title': forms.TextInput(attrs={'class': 'form-input'}),
+                   'description': forms.Textarea(attrs={'class': 'form-area'}),
+                   'status': forms.Select(attrs={'class': 'form-select'}),
+                   'task_type': forms.CheckboxSelectMultiple(attrs={'class': 'radio-btn'})}
 
-class TaskForm(forms.Form):
-    title = forms.CharField(max_length=200, required=True, label='Название:',
-                            widget=forms.TextInput(attrs={'class': 'form-input'}))
-    description = forms.CharField(max_length=2000, required=False, label='Подробное описание:',
-                                  widget=forms.Textarea(attrs={'class': 'form-area'}))
-    status = forms.ModelChoiceField(queryset=Status.objects.all(), required=True, label='Статус:', empty_label=None,
-                                    widget=forms.Select(attrs={'class': 'form-select'}))
-    type = forms.ModelMultipleChoiceField(queryset=Task_type.objects.all(), required=True, label='Тип:',
-                                          widget=forms.CheckboxSelectMultiple(attrs={'class': 'radio-btn'}))
+    #
+    # title = forms.CharField(max_length=200, required=True, label='Название:',
+    #                         widget=forms.TextInput(attrs={'class': 'form-input'}))
+    # description = forms.CharField(max_length=2000, required=False, label='Подробное описание:',
+    #                               widget=forms.Textarea(attrs={'class': 'form-area'}))
+    # status = forms.ModelChoiceField(queryset=Status.objects.all(), required=True, label='Статус:', empty_label=None,
+    #                                 widget=forms.Select(attrs={'class': 'form-select'}))
+    # type = forms.ModelMultipleChoiceField(queryset=Task_type.objects.all(), required=True, label='Тип:',
+    #                                       widget=forms.CheckboxSelectMultiple(attrs={'class': 'radio-btn'}))
