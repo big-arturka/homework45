2124aac04ef549aa8e45d2787acb46023156f026
Arthur <arturkrmnlv10@gmail.com>
2020-08-31 21:13:29 +0600

    Вход и выход переделаны на джанговские.

--- source/main/urls.py
+++ source/main/urls.py
@@ -19,7 +19,7 @@ from django.urls import path
 from webapp.views import IndexView, ProjectView, ProjectCreateView, ProjectUpdateView, ProjectDeleteView
 from webapp.views.task_views import TaskCreateView, TaskView, TaskUpdateView, TaskDeleteView
 
-from accounts.views import login_view, logout_view
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -33,6 +33,6 @@ urlpatterns = [
     path('project/<int:project_pk>/task/<int:pk>/edit/', TaskUpdateView.as_view(), name='task_update'),
     path('project/<int:project_pk>/task/<int:pk>/delete/', TaskDeleteView.as_view(), name='task_delete'),
 
-    path('accounts/login/', login_view, name='login'),
-    path('accounts/logut/', logout_view, name='logout'),
+    path('accounts/login/', LoginView.as_view(), name='login'),
+    path('accounts/logout/', LogoutView.as_view(), name='logout'),
 ]
