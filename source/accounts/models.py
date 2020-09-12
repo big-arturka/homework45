a025391096d0f06133995c34163243e75444f51f
Arthur <arturkrmnlv10@gmail.com>
2020-09-12 19:40:35 +0600

    Создана модель профиля, проведена миграция.

--- source/accounts/models.py
+++ source/accounts/models.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+class Profile(models.Model):
+    user: AbstractUser = models.OneToOneField(get_user_model(), related_name='profile',
+                                              on_delete=models.CASCADE, verbose_name='Пользователь')
+    github = models.CharField(max_length=100, null=True, blank=True, verbose_name='Дата рождения')
+    vita = models.TextField(null=True, blank=True, verbose_name='Аватар')
+    avatar = models.ImageField(null=True, blank=True, upload_to='user_pics', verbose_name='Аватар')
+
+    def __str__(self):
+        return self.user.get_full_name() + "'s Profile"
+
+    class Meta:
+        verbose_name = 'Профиль'
+        verbose_name_plural = 'Профили'
